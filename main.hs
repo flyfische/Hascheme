118dc12bb11751be0c2ff7848cb616e9ddc23e2f
Cyrus Hilliard <monkfische@gmail.com>
2016-03-24 23:10:13 -0400

    added atom evaluations

--- main.hs
+++ main.hs
@@ -11,6 +11,7 @@ spaces = skipMany1 space
 
 data LispVal = Atom String
              | List [LispVal]
+             | DottedList [LispVal] LispVal
              | Number Integer
              | String String
              | Bool Bool
@@ -38,10 +39,51 @@ parseExpr :: Parser LispVal
 parseExpr = parseAtom
         <|> parseString
         <|> parseNumber
+        <|> parseQuoted
+        <|> do char '('
+               x <- (try parseList) <|> parseDottedList
+               char ')'
+               return x
+
+parseList :: Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseDottedList :: Parser LispVal
+parseDottedList = do head <- endBy parseExpr spaces
+                     tail <- char '.' >> spaces >> parseExpr
+                     return $ DottedList head tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do char '\''
+                 x <- parseExpr
+                 return $ List [Atom "quote", x]
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+instance Show LispVal where show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
         
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "list" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value"
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
+
+main :: IO ()
+main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
 
   
